c702f3d4f9fb6203dc69848a24236d8a64f09671
Nick DeRobertis <whoopnip@gmail.com>
2021-11-27 10:48:00 -0500

    Add test for update add new template

--- tests/fixtures/git.py
+++ tests/fixtures/git.py
@@ -2,7 +2,10 @@ import pytest
 from git import Repo
 
 from flexlate.ext_git import stage_and_commit_all
+from flexlate.template.cookiecutter import CookiecutterTemplate
+from flexlate.update.main import Updater
 from tests.dirutils import wipe_generated_folder
+from tests.fixtures.template import cookiecutter_one_template
 from tests.gitutils import (
     create_empty_repo,
     add_dummy_file_to_repo,
@@ -28,4 +31,15 @@ def repo_with_placeholder_committed(empty_generated_repo: Repo) -> Repo:
 def dirty_repo(repo_with_placeholder_committed: Repo) -> Repo:
     repo = repo_with_placeholder_committed
     add_dummy_file2_to_repo(repo)
+    yield repo
+
+
+@pytest.fixture
+def repo_with_template_branch_from_cookiecutter_one(
+    repo_with_placeholder_committed: Repo,
+    cookiecutter_one_template: CookiecutterTemplate,
+) -> Repo:
+    repo = repo_with_placeholder_committed
+    updater = Updater()
+    updater.update(repo, [cookiecutter_one_template])
     yield repo
--- tests/test_update.py
+++ tests/test_update.py
@@ -6,9 +6,12 @@ from flexlate.template.cookiecutter import CookiecutterTemplate
 from flexlate.ext_git import DEFAULT_BRANCH_NAME
 from flexlate.update.main import Updater
 from tests.config import GENERATED_FILES_DIR
-from tests.fileutils import cookiecutter_one_generated_text_content
+from tests.fileutils import (
+    cookiecutter_one_generated_text_content,
+    cookiecutter_two_generated_text_content,
+)
 from tests.fixtures.git import *
-from tests.fixtures.template import cookiecutter_one_template
+from tests.fixtures.template import *
 
 
 def test_update_template_no_history(
@@ -21,7 +24,10 @@ def test_update_template_no_history(
     main_branch: Head = repo.branches["master"]  # type: ignore
     template_branch: Head = repo.branches[DEFAULT_BRANCH_NAME]  # type: ignore
     assert repo.active_branch == main_branch
-    assert repo.commit().message == 'Update flexlate templates\n\none: d512c7e14e83cb4bc8d4e5ae06bb357e\n'
+    assert (
+        repo.commit().message
+        == "Update flexlate templates\n\none: d512c7e14e83cb4bc8d4e5ae06bb357e\n"
+    )
     assert cookiecutter_one_generated_text_content() == ""
     template_branch.checkout()
     assert repo.active_branch == template_branch
@@ -36,6 +42,28 @@ def test_update_template_dirty_repo(
     updater = Updater()
     with pytest.raises(GitRepoDirtyException):
         updater.update(repo, [cookiecutter_one_template])
+
+
+def test_update_add_template(
+    cookiecutter_templates: List[CookiecutterTemplate],
+    repo_with_template_branch_from_cookiecutter_one: Repo,
+):
+    repo = repo_with_template_branch_from_cookiecutter_one
+    updater = Updater()
+    updater.update(repo, cookiecutter_templates)
+    main_branch: Head = repo.branches["master"]  # type: ignore
+    template_branch: Head = repo.branches[DEFAULT_BRANCH_NAME]  # type: ignore
+    assert repo.active_branch == main_branch
+    assert (
+        repo.commit().message
+        == "Update flexlate templates\n\none: d512c7e14e83cb4bc8d4e5ae06bb357e\ntwo: ee5a0b0fd016835199ee7d3e0beb27cb\n"
+    )
+    assert cookiecutter_one_generated_text_content() == ""
+    assert cookiecutter_two_generated_text_content() == "e"
+    template_branch.checkout()
+    assert repo.active_branch == template_branch
+    assert cookiecutter_one_generated_text_content() == ""
+    assert cookiecutter_two_generated_text_content() == "e"
 
 
 # TODO: test for template branch already exists
