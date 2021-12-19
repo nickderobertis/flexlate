d52f71fa82dd2ff04f576914d545308864049db8
Nick DeRobertis <whoopnip@gmail.com>
2021-12-19 18:47:59 -0500

    Always CD into directory when a commit may happen in a test fixture

--- tests/fixtures/git.py
+++ tests/fixtures/git.py
@@ -2,7 +2,8 @@ import pytest
 from git import Repo
 
 from flexlate.ext_git import stage_and_commit_all
-from tests.dirutils import wipe_generated_folder
+from tests.config import GENERATED_REPO_DIR
+from tests.dirutils import wipe_generated_folder, change_directory_to
 from tests.gitutils import (
     create_empty_repo,
     add_dummy_file_to_repo,
@@ -20,21 +21,24 @@ def empty_generated_repo() -> Repo:
 @pytest.fixture
 def repo_with_placeholder_committed(empty_generated_repo: Repo) -> Repo:
     repo = empty_generated_repo
-    add_dummy_file_to_repo(repo)
-    stage_and_commit_all(repo, "Initial commit")
+    with change_directory_to(GENERATED_REPO_DIR):
+        add_dummy_file_to_repo(repo)
+        stage_and_commit_all(repo, "Initial commit")
     yield repo
 
 
 @pytest.fixture
 def dirty_repo(repo_with_placeholder_committed: Repo) -> Repo:
     repo = repo_with_placeholder_committed
-    add_dummy_file2_to_repo(repo)
+    with change_directory_to(GENERATED_REPO_DIR):
+        add_dummy_file2_to_repo(repo)
     yield repo
 
 
 @pytest.fixture
 def repo_with_gitignore(repo_with_placeholder_committed: Repo) -> Repo:
     repo = repo_with_placeholder_committed
-    add_gitignore_and_ignored_file_to_repo(repo)
-    stage_and_commit_all(repo, "Add sample .gitignore with ignored file")
+    with change_directory_to(GENERATED_REPO_DIR):
+        add_gitignore_and_ignored_file_to_repo(repo)
+        stage_and_commit_all(repo, "Add sample .gitignore with ignored file")
     yield repo
