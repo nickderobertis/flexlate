6e1cec568fc613356b067aaef37aeb1439799547
Nick DeRobertis <whoopnip@gmail.com>
2021-12-19 18:13:14 -0500

    Add a utility for showing directory contents while debugging CI

--- tests/dirutils.py
+++ tests/dirutils.py
@@ -2,6 +2,7 @@ import os
 import shutil
 from contextlib import contextmanager
 from pathlib import Path
+from typing import Sequence
 
 from tests.config import GENERATED_FILES_DIR
 
@@ -18,3 +19,22 @@ def change_directory_to(path: Path):
     os.chdir(path)
     yield
     os.chdir(current_path)
+
+
+def display_contents_of_all_files_in_folder(folder: Path, nested: bool = True, ignores: Sequence[str] = (".git",)):
+    outer_divider = "================================================"
+    divider = "--------------------------------------------------"
+    for path in folder.absolute().iterdir():
+        if path.name in ignores:
+            continue
+        if path.is_file():
+            print(f"{outer_divider}\n{path}\n{divider}")
+            try:
+                print(path.read_text())
+            except UnicodeDecodeError as e:
+                print(f"FAILED TO READ TEXT WITH ERROR: {e}")
+            print(f"{divider}\n{outer_divider}")
+        elif path.is_dir():
+            print(f"- {path} (directory)")
+            if nested:
+                display_contents_of_all_files_in_folder(path, nested)
