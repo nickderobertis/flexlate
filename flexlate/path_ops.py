385494659e264796fd8aae106ae69ab60d3493a5
Nick DeRobertis <whoopnip@gmail.com>
2021-12-19 19:03:18 -0500

    Add debugging statements to location relative to parent

--- flexlate/path_ops.py
+++ flexlate/path_ops.py
@@ -54,17 +54,27 @@ def location_relative_to_new_parent(
     orig_parent: Path,
     new_parent: Path,
     path_is_relative_to: Optional[Path] = None,
-):
+) -> Path:
+    print("getting location relative to new parent")
+    print("path", path)
+    print("orig parent", orig_parent)
+    print("new parent", new_parent)
+    print("path is relative to", path_is_relative_to)
     if path_is_relative_to is None and not path.is_absolute():
+        print("hit value error")
         raise ValueError(
             f"must pass path_is_relative_to when passing relative path {path}"
         )
     abs_path: Path = path
     if not path.is_absolute() and path_is_relative_to is not None:
         abs_path = path_is_relative_to.absolute() / path
+    print("abs path is", abs_path)
     try:
-        return new_parent / abs_path.relative_to(orig_parent)
+        result = new_parent / abs_path.relative_to(orig_parent)
+        print("got result", result)
+        return result
     except ValueError as e:
         if "is not in the subpath of" in str(e):
             # Path is not in project, must be user path, return as is
+            print("not in subpath, returning", path)
             return path
