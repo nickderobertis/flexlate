883cae1ed887fa0675378956aa1ba3995ddce32f
Nick DeRobertis <whoopnip@gmail.com>
2022-01-01 13:59:42 -0500

    Refine undo add template source test

--- tests/test_undo.py
+++ tests/test_undo.py
@@ -1,6 +1,8 @@
 from flexlate.transactions.undoer import Undoer
 from tests.fixtures.templated_repo import *
 
+REPO_WITH_COOKIECUTTER_ONE_SOURCE_COMMIT_MESSAGE = 'Added template source one to .\n\n-------------------BEGIN FLEXLATE TRANSACTION-------------------\n{\n  "type": "add source",\n  "target": null,\n  "out_root": null,\n  "data": null,\n  "id": "93f984ca-6e8f-45e9-b9b0-aebebfe798c1"\n}\n'
+
 
 def test_undo_add_template_source(repo_with_cookiecutter_one_template_source: Repo):
     repo = repo_with_cookiecutter_one_template_source
@@ -8,6 +10,7 @@ def test_undo_add_template_source(repo_with_cookiecutter_one_template_source: Re
     config_path = GENERATED_REPO_DIR / "flexlate.json"
     with change_directory_to(GENERATED_REPO_DIR):
         assert config_path.exists()
+        assert repo.commit().message == REPO_WITH_COOKIECUTTER_ONE_SOURCE_COMMIT_MESSAGE
         undoer.undo_transactions(repo)
         assert not config_path.exists()
-        # TODO: check branch history to ensure commits no longer there
+        assert repo.commit().message == "Initial commit\n"
