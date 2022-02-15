85953d5db180da6f5516f723edb18fd3eb8cb3b5
Nick DeRobertis <whoopnip@gmail.com>
2022-02-15 17:16:11 -0500

    Remove temp CI debugging changes

--- flexlate/config_manager.py
+++ flexlate/config_manager.py
@@ -57,13 +57,6 @@ class ConfigManager:
         try:
             config = FlexlateProjectConfig.load_recursive(path)
         except FileNotFoundError:
-            # TEMP
-            print("\n\n\nchecking paths for finding project config\n\n")
-            print("current path exists?", path.exists())
-            print("files in path", path.resolve(), list(path.resolve().iterdir()))
-            print("files in parent", path.resolve().parent, list(path.resolve().parent.iterdir()))
-            print("files in grandparent", path.resolve().parent.parent, list(path.resolve().parent.parent.iterdir()))
-            # END TEMP
             raise FlexlateProjectConfigFileNotExistsException(
                 f"could not find a projects config file in any "
                 f"parent directory of path {path} or in the user directory"
