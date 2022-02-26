07d6782292612ccc9134693c6f98be34e0542a01
Nick DeRobertis <whoopnip@gmail.com>
2022-02-26 14:49:07 -0500

    Also check that passing names works for checker

--- tests/test_checker.py
+++ tests/test_checker.py
@@ -1,20 +1,26 @@
+from typing import Optional
+
 from git import Repo
 
 from flexlate.checker import Checker
 from flexlate.config import FlexlateConfig
 from flexlate.template.copier import CopierTemplate
-from tests.config import COOKIECUTTER_REMOTE_NAME
+from tests.config import COOKIECUTTER_REMOTE_NAME, COPIER_ONE_NAME
 from tests.fixtures.templated_repo import *
 from tests.fixtures.template import *
 from tests.fixtures.transaction import *
 
 
+@pytest.mark.parametrize("names", [None, [COOKIECUTTER_REMOTE_NAME], [COPIER_ONE_NAME]])
 def test_check_for_remote_template_update(
+    names: Optional[List[str]],
     repo_with_cookiecutter_remote_version_one_template_source_and_no_target_version: Repo,
     copier_one_template: CopierTemplate,
     add_source_transaction: FlexlateTransaction,
 ):
-    repo = repo_with_cookiecutter_remote_version_one_template_source_and_no_target_version
+    repo = (
+        repo_with_cookiecutter_remote_version_one_template_source_and_no_target_version
+    )
     template = copier_one_template
     checker = Checker()
     adder = Adder()
@@ -25,6 +31,9 @@ def test_check_for_remote_template_update(
     )
 
     new_versions = checker.find_new_versions_for_template_sources(
-        project_root=GENERATED_REPO_DIR
+        names=names, project_root=GENERATED_REPO_DIR
     )
-    assert new_versions == {COOKIECUTTER_REMOTE_NAME: COOKIECUTTER_REMOTE_VERSION_2}
+    if names is None or names == [COOKIECUTTER_REMOTE_NAME]:
+        assert new_versions == {COOKIECUTTER_REMOTE_NAME: COOKIECUTTER_REMOTE_VERSION_2}
+    else:
+        assert new_versions == {}
