271e5316f58f98c524191e1d589f62ba998eec77
Nick DeRobertis <whoopnip@gmail.com>
2022-02-28 06:02:57 -0500

    Add edge case tests for checker

--- tests/test_checker.py
+++ tests/test_checker.py
@@ -1,9 +1,11 @@
 from typing import Optional
 
+import pytest
 from git import Repo
 
 from flexlate.checker import Checker
 from flexlate.config import FlexlateConfig
+from flexlate.exc import TemplateNotRegisteredException
 from flexlate.template.copier import CopierTemplate
 from tests.config import COOKIECUTTER_REMOTE_NAME, COPIER_ONE_NAME
 from tests.fixtures.templated_repo import *
@@ -37,3 +39,23 @@ def test_check_for_remote_template_update(
         assert new_versions == {COOKIECUTTER_REMOTE_NAME: COOKIECUTTER_REMOTE_VERSION_2}
     else:
         assert new_versions == {}
+
+
+def test_check_for_update_with_no_template_sources(
+    repo_with_placeholder_committed: Repo,
+):
+    checker = Checker()
+    new_versions = checker.find_new_versions_for_template_sources(
+        project_root=GENERATED_REPO_DIR
+    )
+    assert new_versions == {}
+
+
+def test_check_for_update_template_that_does_not_exist(
+    repo_with_placeholder_committed: Repo,
+):
+    checker = Checker()
+    with pytest.raises(TemplateNotRegisteredException):
+        checker.find_new_versions_for_template_sources(
+            names=["some-fake-template"], project_root=GENERATED_REPO_DIR
+        )
