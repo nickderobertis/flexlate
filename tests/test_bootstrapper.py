1653d069d7ca43f9bb8a0ee04f3ef5e83bb1a5d8
Nick DeRobertis <whoopnip@gmail.com>
2022-03-12 17:52:02 -0500

    Add test for bootstrap with conflicts

--- tests/test_bootstrapper.py
+++ tests/test_bootstrapper.py
@@ -1,5 +1,8 @@
+from unittest.mock import patch
+
 from git import Repo
 
+from flexlate import branch_update
 from flexlate.bootstrapper import Bootstrapper
 from flexlate.config import FlexlateProjectConfig
 from tests.fixtures.templated_repo import *
@@ -23,6 +26,42 @@ def test_bootstrap_cookiecutter_one(
     bootstrapper.bootstrap_flexlate_init_from_existing_template(
         repo, template, bootstrap_transaction, no_input=True, data=dict(a="b", c="")
     )
+
+    # Check that all Flexlate config files are correct
+    assert_template_source_cookiecutter_one_added_correctly(cookiecutter_one_template)
+    assert_cookiecutter_one_applied_template_added_correctly(
+        template, GENERATED_REPO_DIR / "b"
+    )
+    assert_project_config_is_correct()
+
+    _assert_flexlate_merge_branch_exists_and_is_up_to_date(repo)
+
+
+def test_bootstrap_cookiecutter_one_with_conflicts(
+    repo_with_cookiecutter_one_applied_but_no_flexlate: Repo,
+    cookiecutter_one_template: CookiecutterTemplate,
+    bootstrap_transaction: FlexlateTransaction,
+):
+    repo = repo_with_cookiecutter_one_applied_but_no_flexlate
+    template = cookiecutter_one_template
+
+    # Modify templated output to cause conflict
+    content_path = GENERATED_REPO_DIR / "b" / "text.txt"
+    content_path.write_text("merge conflict")
+    stage_and_commit_all(
+        repo, "Add a change that should cause a merge conflict on bootstrap"
+    )
+
+    bootstrapper = Bootstrapper()
+
+    def _resolve_conflicts_then_type_yes(prompt: str) -> bool:
+        stage_and_commit_all(repo, "Resolve conflicts")
+        return True
+
+    with patch.object(branch_update, "confirm_user", _resolve_conflicts_then_type_yes):
+        bootstrapper.bootstrap_flexlate_init_from_existing_template(
+            repo, template, bootstrap_transaction, no_input=True, data=dict(a="b", c="")
+        )
 
     # Check that all Flexlate config files are correct
     assert_template_source_cookiecutter_one_added_correctly(cookiecutter_one_template)
