aa06c12cde4add82d68f417bb23d0e6c8b30e5e3
Nick DeRobertis <whoopnip@gmail.com>
2022-04-09 13:25:48 -0400

    Add basic support for passing input in docs terminals

--- docsrc/directives/terminal.py
+++ docsrc/directives/terminal.py
@@ -1,4 +1,6 @@
+import ast
 import os
+import re
 import runpy
 import subprocess
 import tempfile
@@ -6,6 +8,7 @@ from copy import deepcopy
 from pathlib import Path
 from typing import List, Dict, Any, Sequence, Optional, Type
 
+import pexpect
 from docutils.nodes import Node
 from docutils.parsers.rst import directives
 from docutils.statemachine import StringList
@@ -44,21 +47,65 @@ class Command(BaseModel):
 
 
 def _run_commands_in_temp_dir(
-    commands: Sequence[str], setup_command: Optional[str] = None
+    commands: Sequence[str],
+    setup_command: Optional[str] = None,
+    input: Optional[List[str]] = None,
 ) -> List[Command]:
+    input = input or []
     orig_dir = os.getcwd()
     with tempfile.TemporaryDirectory() as tmpdir:
         os.chdir(tmpdir)
         if setup_command:
             # Don't save the output of the setup command
             _run(setup_command)
-        out_commands: List[Command] = [_run(command) for command in commands]
+        out_commands: List[Command] = []
+        for i, command in enumerate(commands):
+            try:
+                this_command_input = input[i]
+            except IndexError:
+                this_command_input = None
+            out_commands.append(_run(command, this_command_input))
         os.chdir(orig_dir)
     return out_commands
 
 
-def _run(command: str) -> Command:
-    output = subprocess.run(command, shell=True, capture_output=True)
+ansi_escape = re.compile(
+    r"""
+    \x1B  # ESC
+    (?:   # 7-bit C1 Fe (except CSI)
+        [@-Z\\-_]
+    |     # or [ for CSI, followed by a control sequence
+        \[
+        [0-?]*  # Parameter bytes
+        [ -/]*  # Intermediate bytes
+        [@-~]   # Final byte
+    )
+""",
+    re.VERBOSE,
+)
+
+
+def _remove_bash_formatting_from_output(output: str) -> str:
+    return ansi_escape.sub("", output)
+
+
+def _run(command: str, input: Optional[str] = None) -> Command:
+    use_input = input.split("\n") if input else None
+    stop_for_input_chars = ["]: ", r"0m: "]
+
+    if use_input:
+        process = pexpect.spawn(f'bash -c "{command}"', encoding="utf-8")
+        all_stdout = ""
+        for inp in use_input:
+            process.expect_exact(stop_for_input_chars, timeout=10)
+            this_stdout = process.before + process.after
+            all_stdout += _remove_bash_formatting_from_output(this_stdout)
+            process.sendline(inp)
+        process.expect(pexpect.EOF)
+        all_stdout += _remove_bash_formatting_from_output(process.before)
+        return Command(input=command, output=all_stdout)
+
+    output = subprocess.run(command, shell=True, capture_output=True, input=use_input)
     return Command(
         input=command, output=output.stdout.decode() + "\n" + output.stderr.decode()
     )
@@ -72,9 +119,17 @@ def _commands_to_list(commands: Sequence[Command]) -> List[str]:
     return output
 
 
+def _get_list(input: str) -> List[str]:
+    if not input.startswith("["):
+        if input:
+            return [input]
+        return []
+    return ast.literal_eval(input)
+
+
 class RunTerminalDirective(SphinxDirective):
     required_arguments = 0
-    option_spec = {"setup": directives.unchanged}
+    option_spec = {"setup": directives.unchanged, "input": _get_list}
     has_content = True
     always_setup_commands: List[str] = []
 
@@ -85,7 +140,10 @@ class RunTerminalDirective(SphinxDirective):
         else:
             use_commands = self.always_setup_commands
         full_setup_command: str = " && ".join(use_commands)
-        output = _run_commands_in_temp_dir(self.content, full_setup_command)
+        input: List[str] = self.options.get("input", [])
+        output = _run_commands_in_temp_dir(
+            self.content, full_setup_command, input=input
+        )
         text = _commands_to_list(output)
         return [termy_block(self.env.myst_config, text)]
 
