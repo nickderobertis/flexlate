56a0f9c6b605e4c7b84bdd2abd452545aaf2c5c5
Nick DeRobertis <whoopnip@gmail.com>
2022-04-09 10:22:20 -0400

    Add setup command option

--- docsrc/directives/terminal.py
+++ docsrc/directives/terminal.py
@@ -4,9 +4,10 @@ import subprocess
 import tempfile
 from copy import deepcopy
 from pathlib import Path
-from typing import List, Dict, Any, Sequence
+from typing import List, Dict, Any, Sequence, Optional
 
 from docutils.nodes import Node
+from docutils.parsers.rst import directives
 from docutils.statemachine import StringList
 from myst_parser import create_myst_config
 from myst_parser.docutils_renderer import DocutilsRenderer
@@ -42,10 +43,15 @@ class Command(BaseModel):
     output: str
 
 
-def _run_commands_in_temp_dir(commands: Sequence[str]) -> List[Command]:
+def _run_commands_in_temp_dir(
+    commands: Sequence[str], setup_command: Optional[str] = None
+) -> List[Command]:
     orig_dir = os.getcwd()
     with tempfile.TemporaryDirectory() as tmpdir:
         os.chdir(tmpdir)
+        if setup_command:
+            # Don't save the output of the setup command
+            _run(setup_command)
         out_commands: List[Command] = [_run(command) for command in commands]
         os.chdir(orig_dir)
     return out_commands
@@ -68,9 +74,11 @@ def _commands_to_list(commands: Sequence[Command]) -> List[str]:
 
 class RunTerminalDirective(SphinxDirective):
     required_arguments = 0
+    option_spec = {"setup": directives.unchanged}
     has_content = True
 
     def run(self) -> List[Node]:
-        output = _run_commands_in_temp_dir(self.content)
+        setup_command: str = self.options.get("setup", "")
+        output = _run_commands_in_temp_dir(self.content, setup_command)
         text = _commands_to_list(output)
         return [termy_block(self.env.myst_config, text)]
