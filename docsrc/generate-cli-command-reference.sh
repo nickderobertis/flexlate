307a36d360e3ea757049218eb2aba5bbdda7bd92
Nick DeRobertis <whoopnip@gmail.com>
2022-04-10 15:57:26 -0400

    Get generating command reference pointing to current source

--- docsrc/generate-cli-command-reference.sh
+++ docsrc/generate-cli-command-reference.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd ..
-typer flexlate.cli utils docs --name fxt --output docsrc/source/commands.md
+typer flexlate/cli.py utils docs --name fxt --output docsrc/source/commands.md
 cd docsrc
