33337c8d0664e0294d6d8795e0422c9bef8b5aa6
Nick DeRobertis <whoopnip@gmail.com>
2022-04-15 11:25:46 -0400

    Add a currently failing test that catches issue of not being able to init from current directory

--- tests/integration/test_main.py
+++ tests/integration/test_main.py
@@ -2,6 +2,7 @@ import os.path
 
 from flexlate.main import Flexlate
 from flexlate.template.types import TemplateType
+from tests.config import GENERATED_FILES_DIR
 from tests.fixtures.templated_repo import *
 
 fxt = Flexlate()
@@ -29,3 +30,39 @@ def test_add_template_source_from_current_path(
     assert source.type == TemplateType.COOKIECUTTER
     assert source.render_relative_root_in_output == Path("{{ cookiecutter.a }}")
     assert source.render_relative_root_in_template == Path("{{ cookiecutter.a }}")
+
+
+def test_init_from_current_path(
+    repo_with_placeholder_committed: Repo,
+    cookiecutter_one_template: CookiecutterTemplate,
+):
+    template = cookiecutter_one_template
+    project_dir = GENERATED_FILES_DIR / "project"
+
+    with change_directory_to(template.path):
+        fxt.init_project_from(".", path=GENERATED_FILES_DIR, no_input=True)
+
+    content_path = project_dir / "a1.txt"
+    content = content_path.read_text()
+    assert content == "1"
+
+    readme_path = project_dir / "README.md"
+    assert readme_path.read_text() == "some existing content"
+
+    config_path = project_dir / "flexlate.json"
+    config = FlexlateConfig.load(config_path)
+    assert len(config.template_sources) == 1
+    source = config.template_sources[0]
+    assert source.name == template.name
+    assert source.path == str(template.path)
+    assert source.version == template.version
+    assert source.type == TemplateType.COPIER
+    assert source.render_relative_root_in_output == Path(".")
+    assert source.render_relative_root_in_template == Path(".")
+    assert len(config.applied_templates) == 1
+    at = config.applied_templates[0]
+    assert at.name == template.name
+    assert at.version == template.version
+    assert at.data == {"q1": "a1", "q2": 1, "q3": None}
+    assert at.root == Path(".")
+    assert at.add_mode == AddMode.LOCAL
