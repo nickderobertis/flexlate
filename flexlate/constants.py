8a33e7e13795fc0a16df54b882664d45bfd2f1a5
Nick DeRobertis <whoopnip@gmail.com>
2022-05-29 08:07:55 -0400

    chore: Try to get windows working on Python 3.10 and greater via ignore_cleanup_errors. Disable tests for Windows Python 3.8/9

--- flexlate/constants.py
+++ flexlate/constants.py
@@ -1,2 +1,6 @@
+import sys
+
 DEFAULT_MERGED_BRANCH_NAME = "flexlate-output"
 DEFAULT_TEMPLATE_BRANCH_NAME = "flexlate-templates"
+
+PY10 = sys.version_info >= (3, 10)
--- flexlate/temp_path.py
+++ flexlate/temp_path.py
@@ -3,6 +3,8 @@ import tempfile
 from pathlib import Path
 from typing import Iterator
 
+from flexlate.constants import PY10
+
 
 @contextlib.contextmanager
 def create_temp_path() -> Iterator[Path]:
@@ -13,7 +15,14 @@ def create_temp_path() -> Iterator[Path]:
     1. That returns a string and not a path
     2. On MacOS, the temp directory has a symlink. This resolves the symlink so that
        there won't be any mismatch in resolved paths.
+    3. On Windows, the temp directory can fail to delete with a PermissionError. On Python
+       3.10 and greater, there is a ignore_cleanup_errors parameter that can be added to
+       tempfile.TemporaryDirectory to ignore the error. This function passes this
+       parameter if we are running on Python 3.10 or greater.
     """
-    with tempfile.TemporaryDirectory() as temp_dir:
+    kwargs = {}
+    if PY10:
+        kwargs["ignore_cleanup_errors"] = True
+    with tempfile.TemporaryDirectory(**kwargs) as temp_dir:
         path = Path(temp_dir).resolve()
         yield path
