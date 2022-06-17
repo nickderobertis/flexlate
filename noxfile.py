915bcd16df0c3a64787db57c47359cb17d837965
Flexlate <flexlate@nickderobertis.com>
2022-06-17 03:52:55 +0000

    Update flexlate templates
    copier-pypi-sphinx-flexlate: 96b1b0ca199250f843ab60bbd742f59c0d0286a3
    copier-flexlate-github-actions: 9743929a6beca33d162bdb936a6f462b6ed417d0
    
    -------------------BEGIN FLEXLATE TRANSACTION-------------------
    {
      "type": "update",
      "target": "",
      "out_root": null,
      "data": null,
      "id": "3f989922-2cbd-4fa0-8ab1-a3b788531b42"
    }

--- noxfile.py
+++ noxfile.py
@@ -1,6 +1,20 @@
+import os
+import platform
+import shutil
+from pathlib import Path
+from typing import Literal
+
 import nox
 
 nox.options.sessions = ["format", "strip", "lint", "test"]
+
+VENVS_DIR = Path("~").expanduser() / ".venvs"
+PROJECT_NAME = Path(__file__).parent.name
+PROJECT_VENV_DIR = VENVS_DIR / PROJECT_NAME
+LINT_VENV_DIR = PROJECT_VENV_DIR / "lint"
+LINT_VENV_DIR_STR = str(LINT_VENV_DIR)
+
+VenvName = Literal["lint"]
 
 
 @nox.session(python=False)
@@ -36,10 +50,15 @@ def _format_in_place(session, files):
 
 @nox.session(python=False)
 def lint(session):
-    session.run(
+    _setup_venv(session, "lint")
+
+    def run_lint(*args):
+        _run_in_venv(session, "lint", *args)
+
+    run_lint(
         "flake8", "--count", "--select=E9,F63,F7,F82", "--show-source", "--statistics"
     )
-    session.run(
+    run_lint(
         "flake8",
         "--count",
         "--exit-zero",
@@ -47,7 +66,7 @@ def lint(session):
         "--max-line-length=127",
         "--statistics",
     )
-    session.run("mypy")
+    run_lint("mypy")
 
 
 @nox.session(python=False, name="strip")
@@ -90,5 +109,87 @@ def docs(session):
     session.chdir("docsrc")
     session.run("make", "github")
     if session.interactive:
-        session.run("ls", "-l")
         session.run("bash", "./dev-server.sh")
+
+
+def _setup_venv(session, venv_name: VenvName):
+    venv_path = _venv_path(venv_name)
+    if _venv_exists(venv_name):
+        print(f"Using existing {venv_name} venv at {venv_path}")
+        return
+    session.run("virtualenv", str(venv_path))
+    _update_venv(session, venv_name)
+
+
+def _venv_path(venv_name: VenvName) -> Path:
+    return PROJECT_VENV_DIR / venv_name
+
+
+def _venv_exists(venv_name: VenvName):
+    venv_dir = _venv_path(venv_name)
+    return venv_dir.exists()
+
+
+@nox.session(python=False)
+def venv(session):
+    # First argument is one of "delete", "update", "create"
+    # Second argument is the name of the venv
+    if len(session.posargs) < 2:
+        raise ValueError(
+            f"Must supply first action (delete, update, create), then venv name, got {session.posargs}"
+        )
+    action = session.posargs[0]
+    venv_name = session.posargs[1]
+    if action == "delete":
+        _delete_venv(venv_name)
+    elif action == "update":
+        _update_venv(session, venv_name)
+    elif action == "create":
+        _setup_venv(session, venv_name)
+    else:
+        raise ValueError(f"Action must be delete or update, got {action}")
+
+
+def _run_in_venv(session, venv_name: VenvName, *args):
+    new_command = _venv_command(venv_name, *args)
+    session.run(*new_command)
+
+
+def _venv_command(venv_name: VenvName, *args):
+    if platform.system() == "Windows":
+        return _venv_command_windows(venv_name, *args)
+    return _venv_command_unix(venv_name, *args)
+
+
+def _venv_command_unix(venv_name: VenvName, *args):
+    venv_dir = _venv_path(venv_name)
+    venv_command = os.path.sep.join((str(venv_dir), "bin", args[0]))
+    new_args = [venv_command, *args[1:]]
+    return new_args
+
+
+def _venv_command_windows(venv_name: VenvName, *args):
+    venv_dir = _venv_path(venv_name)
+    venv_command = os.path.sep.join((str(venv_dir), "Scripts", args[0] + ".exe"))
+    new_args = [venv_command, *args[1:]]
+    return new_args
+
+
+def _delete_venv(venv_name: VenvName):
+    venv_dir = _venv_path(venv_name)
+    if venv_dir.exists():
+        shutil.rmtree(venv_dir)
+        print(f"Deleted venv {venv_name} from {venv_dir}")
+    else:
+        raise ValueError(f"Venv {venv_name} does not exist at {venv_dir}")
+
+
+def _update_venv(session, venv_name: VenvName):
+    venv_dir = _venv_path(venv_name)
+    if not venv_dir.exists():
+        raise ValueError(
+            f"Venv {venv_name} does not exist at {venv_dir}, cannot update"
+        )
+    _run_in_venv(
+        session, venv_name, "pip", "install", "-r", f"{venv_name}-requirements.txt"
+    )
